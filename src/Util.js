2ae11fb86e1bcd8a8c478d2969b11547121953b3
beomhw <qjatlr6919@gmail.com>
2021-05-24 09:15:37 +0900

    STATE : util 수정

--- src/Util.js
+++ src/Util.js
@@ -6,17 +6,6 @@ export const KEYWORD = process.env.REACT_APP_KEYWORD;
 export const URL = 'https://api.themoviedb.org/3/';
 export const IMG_URL = 'http://image.tmdb.org/t/p/w500';
 
-export const Request = {
-    all: '',
-    characters_staff: 'characters_staff',
-    episodes: 'episodes',
-    news: 'news',
-    pictures: 'pictures',
-    videos: 'videos',
-    stats: 'stats',
-    reviews: 'reviews'
-}
-
 // 테마 관련
 export const BACKGROUND_LI = '#e9ecef';
 export const BACKGROUND_DA = '#1b2021';
