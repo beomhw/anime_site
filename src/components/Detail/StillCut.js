69d7452b4a430425e00e88f0c91576a52b046c99
beomhw <qjatlr6919@gmail.com>
2021-06-03 08:47:35 +0900

    Fixed : Youtube 수정

--- src/components/Detail/StillCut.js
+++ src/components/Detail/StillCut.js
@@ -17,8 +17,8 @@ const YoutubeStyle = styled(YouTube)`
     max-width: 500px;
     max-height: 280px;
     @media(max-width: 500px) {
-        max-width: 400px;
-        max-height: 220px;
+        max-width: 100%;
+        height: 200px;
     }
 `;
 
